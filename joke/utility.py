d5fe5273c7af1f0bd841df0c25615c98b738de76
Mingle Chang <mingle890805@gmail.com>
2016-06-05 21:17:42 +0800

    not

--- joke/utility.py
+++ joke/utility.py
@@ -1,7 +1,9 @@
 # -*- coding: UTF-8 -*-
 import json
 
-
+#status说明
+#200：成功
+#404：接口不存在
 def apiJson(status=404,message=None,data=None,**kwargs):
 	dic={'status':status,'message':message,'result':data}
 	dic.update(kwargs)
